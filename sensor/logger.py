cbff55a11fa2fc391e1e480ebfd03624273e82cf
pompy001 <janbandhu.ankit786@gmail.com>
2023-05-06 14:31:58 +0530

    added exception and testing logging and exception done

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import datetime import datetime
+from datetime import datetime
 
 #log file name
 LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
@@ -17,5 +17,5 @@ LOG_FILE_PATH = os.path.join(LOG__FILE_DIR,LOG_FILE_NAME)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format = "[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
