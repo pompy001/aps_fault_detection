a2c5c215de95d77fa022b049965d551ad70e6a28
pompy001 <janbandhu.ankit786@gmail.com>
2023-05-06 15:53:47 +0530

    data ingestion half done

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 import sys,os
 from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
 
 if __name__=="__main__":
      try:
-          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config =  config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+
      except Exception as e:
           print(e)
